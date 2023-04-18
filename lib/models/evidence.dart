ea1e1ad4b80a66c94b6e0ae96f8173599e3b0397
ArthurCorbellini <arthur.corbellini27@gmail.com>
2023-04-17 22:08:52 -0300

    versão inicial do feed

---
+++ lib/models/evidence.dart
@@ -0,0 +1,11 @@
+class Evidence {
+  const Evidence({
+    required this.id,
+    required this.author,
+    required this.description,
+  });
+
+  final int id;
+  final String author;
+  final String description;
+}
